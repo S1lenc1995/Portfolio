9371635c6eb402a1fde5dd402a8aa8c4d23e4fe9
Bohdan Kletskyi <underwater3499@gmail.com>
2023-03-02 12:12:11 +0200

    work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,11 +11,11 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router>
+    <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </Router>
+    </BrowserRouter>
   </Provider>
 
 
